429a9d94838e00f1edd0eadf2278ec0ae81a8ab5
Yukio Ugajin <jinugasachio@gmail.com>
2021-10-30 23:56:45 +0900

    installスクリプトを追加

---
+++ install.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh -e
+
+: 'setup environment' && {
+  export DOTPATH=$(pwd)
+  mkdir ~/.zsh
+}
+
+: 'install zshrc' && {
+  ln -s $DOTPATH/.zshrc ~/.zshrc
+  for FILE in $(find $DOTPATH/.zsh/ -type f);
+  do
+    FILENAME=$(basename $FILE)
+    echo load $FILENAME ...
+    ln -s $DOTPATH/.zsh/$FILENAME ~/.zsh/$FILENAME
+  done
+}
