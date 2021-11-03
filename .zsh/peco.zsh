5ae0b6db437dae91a2da18d512dfe8581257f0ef
Yukio Ugajin <jinugasachio@gmail.com>
2021-11-03 22:08:41 +0900

    -m追記

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -11,3 +11,20 @@ function peco-select-history() {
 }
 zle -N peco-select-history
 bindkey '^r' peco-select-history
+
+function peco-switch-branch() {
+    local selected_line="$(git for-each-ref --format='%(refname:short) | %(committerdate:relative) | %(committername) | %(subject)' --sort=-committerdate refs/heads refs/remotes \
+        | column -t -s '|' \
+        | grep -v 'origin' \
+        | peco \
+        | head -n 1 \
+        | awk '{print $1}')"
+    if [ -n "$selected_line" ]; then
+        BUFFER="git switch ${selected_line}"
+        CURSOR=$#BUFFER
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-switch-branch
+bindkey '^b' peco-switch-branch
