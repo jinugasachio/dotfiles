0236bbc8a73834778d2d5fed268d1573d3caa531
Yukio Ugajin <jinugasachio@gmail.com>
2021-11-30 22:20:59 +0900

    function追加

---
+++ .zsh/function.zsh
@@ -0,0 +1,26 @@
+gbdelete() {
+  command="git branch | grep -v '*'"
+  for branch_name in "$@"; do
+    command="${command} | grep -v ${branch_name}"
+  done
+  echo 'These branches will be deleted.'
+  eval ${command}
+  verify_execution
+  if [ $? -eq 0 ] ; then
+    command="${command} | xargs git branch -D"
+    eval ${command}
+  fi
+}
+
+verify_execution() {
+  echo "Are you sure[y/n]"
+  read answer
+  case $answer in
+    y)
+    true
+    ;;
+    *)
+    false
+    ;;
+  esac
+}
