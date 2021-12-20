e84c37dba477c30e4612bb39c920f741727b400b
yukio-ugajin <yukio-ugajin@c-fo.com>
2021-12-20 18:46:20 +0900

    修正

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,7 +1,6 @@
 autoload -Uz compinit && compinit                           # compsysで用意されている全ての補完機能を有効にする
-complete -C '/usr/local/bin/aws_completer' aws              # awscliの補完
-
 autoload -U +X bashcompinit && bashcompinit
+complete -C '/usr/local/bin/aws_completer' aws              # awscliの補完
 complete -o nospace -C /usr/local/bin/terraform terraform
 fpath=(~/.zsh/completion $fpath)
 source <(kubectl completion zsh)
